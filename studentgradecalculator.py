f2adec6df4b7a8e835eea87dbd7c5d9f4e526a08
sirvilalitha3 <sirvilalitha3@gmail.com>
2026-08-18 20:54:33 +0530

    Add files via upload

---
+++ studentgradecalculator.py
@@ -0,0 +1,20 @@
+print('\nSTUDENT GRADE CALCULATOR')
+student=input("enter student name:   ")
+marks=float(input("enter student total marks:   "))
+
+if marks>=90:
+        print("O  - eww topperrrrr")
+elif marks>=80:
+        print("A+")
+elif marks>=65:
+        print("A")
+elif marks>=50:
+        print(" B+")
+elif marks>=40:
+        print("B")
+        
+else:
+        print("fail")
+        print("failure is a part of the journey to success")
+        
+
